19c9aaa6a44a728b573f2288345cb41d84fe0689
Ash Isbitt <AIsbitt@tcadmins-MacBook-Air-2.local>
2020-03-13 09:45:45 +0000

    Fixed reading one

--- run_file.py
+++ run_file.py
@@ -6,6 +6,7 @@ while True:
     print("Select 1 for all products")
     print("Select 2 for one product")
     print("Select 3 to insert product")
+    print("Select 4 to delete product")
 
     user_input = input(">>> ")
 
@@ -22,7 +23,8 @@ while True:
 
     elif user_input == "2":
         entry = input("Enter row: ")
-        record = data = products_tb.read_one(entry)
+        data = products_tb.read_one(entry)
+        record = data.fetchone()
         print(record)
 
     elif user_input == "3":
