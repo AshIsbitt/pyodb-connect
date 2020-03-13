0761cd3ef84f4578660a6569d4cd02fa8c320c23
Ash Isbitt <AIsbitt@tcadmins-MacBook-Air-2.local>
2020-03-13 09:39:48 +0000

    methods mostly written

--- db_products.py
+++ db_products.py
@@ -6,22 +6,25 @@ class NWProducts(MSDBconn):
         super().__init__()
         self.table = 'PRODUCTS'
 
-    # This is encpasulated to prevent injection attacks
+    # This is encapsulated to prevent injection attacks
     def __sql_query(self, query):
         return self.crsr.execute(query)
 
     def read_one(self, product_id):
-        pass
+        row = self.__sql_query("SELECT * FROM {} WHERE ProductID = {}".format(self.table, product_id))
+        return row
 
     def read_all(self):
         rows = self.__sql_query("SELECT * FROM {}".format(self.table))
         return rows
 
-    def create(self):
-        #insert into
-        # search for pyodbc commit
-        pass
+    def create_record(self, product_name):
+        row = self.__sql_query("INSERT INTO {} VALUES '{}'".format(self.table, product_name))
+        MSDBconn.conn.commit()
+        return row
 
-    def delete_record(self):
-        pass
+    def delete_record(self, delete):
+        row = self.__sql_query("DELETE FROM {} WHERE ProductID = {}".format(self.table, delete))
+        MSDBconn.conn.commit()
+        return row + " remove"
 
--- run_file.py
+++ run_file.py
@@ -3,7 +3,9 @@ from db_products import *
 products_tb = NWProducts()
 
 while True:
-    print("Select 1 for all products: ")
+    print("Select 1 for all products")
+    print("Select 2 for one product")
+    print("Select 3 to insert product")
 
     user_input = input(">>> ")
 
@@ -17,3 +19,16 @@ while True:
                 break
             else:
                 print(records)
+
+    elif user_input == "2":
+        entry = input("Enter row: ")
+        record = data = products_tb.read_one(entry)
+        print(record)
+
+    elif user_input == "3":
+        product_name = input("Enter new product name: ")
+        print(products_tb.create_record(product_name))
+
+    elif user_input == "4":
+        delete = input("What row ID do you want to delete? ")
+        print(products_tb.delete_record(delete))
